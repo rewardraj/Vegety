721b0b7e236c498f4c9e8a4feceb1ee26a0c8795
Reward Edoyugbo <rewardraj@gmail.com>
2023-02-27 07:51:09 -0800

    updated testimonial section

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,90 +1,167 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import happyCustomer1 from '../../assets/images/customerRed.jpg';
+import { faStar, faArrowRightLong, faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
+import customer1 from '../../assets/images/customer1.jpeg';
+import customer2 from '../../assets/images/customer2.jpeg';
+import customer3 from '../../assets/images/customer3.jpeg';
+import customer4 from '../../assets/images/customerRed.jpg';
 
 const Testimonial = () => {
+  const images = [customer1, customer2, customer3, customer4];
+
+  const reviews = [
+    {
+      text: 'Awesome food, value for the money',
+      color: '#eaf1ef',
+      stars: 4,
+    },
+    {
+      text: 'Great service and delicious food!',
+      color: '#ffe6cc',
+      stars: 5,
+    },
+    {
+      text: 'The best restaurant in town!',
+      color: '#eaf1ef',
+      stars: 5,
+    },
+    {
+      text: 'Awesome food, value for the money',
+      color: '#eaf1ef',
+      stars: 5,
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveIndex((activeIndex + 1) % reviews.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [activeIndex, reviews.length]);
+
+  const activeReview = reviews[activeIndex];
+  const activeImage = images[activeIndex];
+
+
   return (
     <section className='testimonial__Control'>
       <div className='testimonial__Header'>
         <h3 className='header__Intro' style={{
           fontSize: 'var(--text-md)',
-          textAlign: 'left',
+          textAlign: 'center',
           fontWeight: '700',
-          padding: 'var(--gutter) var(--gutter)',
-          lineHeight: 'var(--line-height-default)',
-          width: 'max-content'
+          padding: 'var(--default)',
+          lineHeight: 'var(--line-height-lg)',
         }}>Trusted by thousands <br /> of customers</h3>
       </div>
       <div className='testimonial__Content' style={{
         display: 'flex',
         width: '100%',
         gap: 'var(--gutter)',
+        padding: 'var(--gutter) var(--whitespace)',
       }}>
-        <div className='testimonial__Left' style={{flexBasis: '40%'}}>
-          <div className='left' style={{display: 'flex', flexDirection:'column', gap:'20px'}}>
+        <div className='testimonial__Left' style={{ flexBasis: '40%' }}>
+          <div className='left' style={{
+            display: 'flex', flexDirection: 'column', gap: '20px',
+            padding: 'var(--gutter) var(--gutter)',
+          }}>
             <div className='first' style={{
-              fontSize: 'var(--text-sm)',
+              fontSize: '16px',
               textAlign: 'left',
-              padding: 'var(--default) var(--gutter)',
+              padding: '1rem',
               borderRadius: '10px',
               boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
               lineHeight: 'var(--line-height-default)',
-              width:'max-content',
-              backgroundColor: '#eaf1ef' 
+              width: 'max-content',
+              backgroundColor: activeReview.color
             }}>
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <p>"Awesome food, value for the money"</p>
+              {Array.from({ length: activeReview.stars }).map((_, index) => (
+                <FontAwesomeIcon key={index} icon={faStar} color='gold' />
+              ))}
+              <p>{activeReview.text}</p>
             </div>
 
             <div className='opposite' style={{
-              fontSize: 'var(--text-sm)',
+              fontSize: '18px',
               textAlign: 'left',
               marginLeft: 'auto',
               marginRight: '0',
-              padding: 'var(--gutter) var(--gutter)',
+              padding: '1rem',
               borderRadius: '10px',
+              color: '#fff',
               boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
               lineHeight: 'var(--line-height-default)',
-              width:'max-content',
-              backgroundColor: '#eaf1ef' 
+              width: 'max-content',
+              backgroundColor: 'var(--olive-drab)'
             }}>
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <p>"Awesome food, value for the money"</p>
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <p>Professionalism at it's peak!</p>
             </div>
-            
+
 
             <div className='first' style={{
-              fontSize: 'var(--text-sm)',
+              fontSize: '18px',
               textAlign: 'left',
-              padding: 'var(--gutter) var(--gutter)',
+              padding: 'var(--default) var(--default)',
               borderRadius: '10px',
               boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
               lineHeight: 'var(--line-height-default)',
-              width:'max-content',
-              backgroundColor: '#eaf1ef' 
+              width: 'max-content',
+              backgroundColor: activeReview.color
             }}>
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <FontAwesomeIcon  icon={faStar} color='gold' />
-              <p>"Awesome food, value for the money"</p>
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <FontAwesomeIcon icon={faStar} color='gold' />
+              <p>Best customer service in the area 😊</p>
             </div>
           </div>
-          
+
         </div>
         <div className='testimonial__Right'>
-          <div className='right image'>
-            <img src={happyCustomer1} height={450} width={600} aria-hidden alt='happy customer dark'/>
+          <div className='right image' style={{
+            position: 'relative'
+          }}>
+            <img src={activeImage} height={450} width={550}
+              aria-hidden alt='happy customer dark'
+              style={{
+                borderRadius: '25px',
+                objectFit: 'cover',
+                objectPosition: 'center'
+              }}
+            />
+
+            <div style={{
+              display: 'flex',
+              gap: '20px',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: 'var(--gutter) var(--gutter)',
+
+
+            }}>
+              <FontAwesomeIcon
+                icon={faArrowLeftLong}
+                fontSize='20px'
+                cursor='pointer'
+                onClick={() => setActiveIndex((activeIndex - 1 + reviews.length) % reviews.length)}
+              />
+              <FontAwesomeIcon
+                icon={faArrowRightLong}
+                fontSize='20px'
+                cursor='pointer'
+                onClick={() => setActiveIndex((activeIndex + 1 + reviews.length) % reviews.length)}
+              />
+            </div>
+
           </div>
-          <div className='left top'></div>
-          <div className='left bottom'></div>
+
         </div>
       </div>
     </section>
