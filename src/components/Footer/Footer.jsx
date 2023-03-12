0c28e329b620c5026d4ee6868e6667d9bc862d9a
Reward Edoyugbo <babyraj@Rewards-MacBook-Pro.local>
2023-03-12 21:29:11 +0100

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className='footer__Wrapper'>
+      <div className='social__Buttons'>
+        <FontAwesomeIcon icon={faFacebook} color='#1877f2' size={'3x'}/>
+        <FontAwesomeIcon icon={faInstagram} color='#e4405f' size={'3x'}/>
+        <FontAwesomeIcon icon={faWhatsapp} color='#25D366' size={'3x'} /> 
+        <FontAwesomeIcon icon={faYoutube} color='#ff0000' size={'3x'} /> 
+      </div>
+      <div className='addon__Links'>
+        <p className='addOns'>Home |</p>
+        <p className='addOns'>About Us |</p>
+        <p className='addOns'>Menu |</p>
+        <p className='addOns'>Blog |</p>
+        <p className='addOns'>Contact</p>
+      </div>
+      <div className='appStore__Wrapper'>
+
+      </div>
     </div>
   )
 }
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faArrowRightLong, faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faArrowCircleLeft, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
 import customer1 from '../../assets/images/customer1.jpeg';
 import customer2 from '../../assets/images/customer2.jpeg';
 import customer3 from '../../assets/images/customer3.jpeg';
@@ -23,7 +23,7 @@ const Testimonial = () => {
     {
       text: 'The best restaurant in town!',
       color: '#eaf1ef',
-      stars: 5,
+      stars: 4,
     },
     {
       text: 'Awesome food, value for the money',
@@ -47,121 +47,125 @@ const Testimonial = () => {
 
 
   return (
-    <section className='testimonial__Control'>
-      <div className='testimonial__Header'>
-        <h3 className='header__Intro' style={{
-          fontSize: 'var(--text-md)',
-          textAlign: 'center',
-          fontWeight: '700',
-          padding: 'var(--default)',
-          lineHeight: 'var(--line-height-lg)',
-        }}>Trusted by thousands <br /> of customers</h3>
-      </div>
-      <div className='testimonial__Content' style={{
-        display: 'flex',
-        width: '100%',
-        gap: 'var(--gutter)',
-        padding: 'var(--gutter) var(--whitespace)',
-      }}>
-        <div className='testimonial__Left' style={{ flexBasis: '40%' }}>
-          <div className='left' style={{
-            display: 'flex', flexDirection: 'column', gap: '20px',
-            padding: 'var(--gutter) var(--gutter)',
+    <section className='testimonial__Section'>
+      <div className='testimonial__Control'>
+        <div className='testimonial__Header'>
+          <h3 className='header__Intro' style={{
+            fontSize: 'var(--text-md)',
+            textAlign: 'center',
+            alignItems: 'center',
+            padding: 'var(--default)',
+            lineHeight: 'var(--line-height-lg)',
           }}>
-            <div className='first' style={{
-              fontSize: '16px',
-              textAlign: 'left',
-              padding: '1rem',
-              borderRadius: '10px',
-              boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
-              lineHeight: 'var(--line-height-default)',
-              width: 'max-content',
-              backgroundColor: activeReview.color
-            }}>
-              {Array.from({ length: activeReview.stars }).map((_, index) => (
-                <FontAwesomeIcon key={index} icon={faStar} color='gold' />
-              ))}
-              <p>{activeReview.text}</p>
-            </div>
-
-            <div className='opposite' style={{
-              fontSize: '18px',
-              textAlign: 'left',
-              marginLeft: 'auto',
-              marginRight: '0',
-              padding: '1rem',
-              borderRadius: '10px',
-              color: '#fff',
-              boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
-              lineHeight: 'var(--line-height-default)',
-              width: 'max-content',
-              backgroundColor: 'var(--olive-drab)'
-            }}>
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <p>Professionalism at it's peak!</p>
-            </div>
-
-
-            <div className='first' style={{
-              fontSize: '18px',
-              textAlign: 'left',
-              padding: 'var(--default) var(--default)',
-              borderRadius: '10px',
-              boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
-              lineHeight: 'var(--line-height-default)',
-              width: 'max-content',
-              backgroundColor: activeReview.color
-            }}>
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <FontAwesomeIcon icon={faStar} color='gold' />
-              <p>Best customer service in the area 😊</p>
-            </div>
-          </div>
-
+            Trusted by thousands of customers</h3>
         </div>
-        <div className='testimonial__Right'>
-          <div className='right image' style={{
-            position: 'relative'
-          }}>
-            <img src={activeImage} height={450} width={550}
-              aria-hidden alt='happy customer dark'
-              style={{
-                borderRadius: '25px',
-                objectFit: 'cover',
-                objectPosition: 'center'
-              }}
-            />
 
-            <div style={{
-              display: 'flex',
-              gap: '20px',
-              justifyContent: 'center',
-              alignItems: 'center',
+        <div className='testimonial__Content' style={{
+          display: 'flex',
+          width: '100%',
+          gap: 'var(--gutter)',
+          padding: 'var(--whitespace) var(--gutter)',
+        }}>
+          <div className='testimonial__Left' style={{ flexBasis: '40%' }}>
+            <div className='left' style={{
+              display: 'flex', flexDirection: 'column', gap: '20px',
               padding: 'var(--gutter) var(--gutter)',
-
-
             }}>
-              <FontAwesomeIcon
-                icon={faArrowLeftLong}
-                fontSize='20px'
-                cursor='pointer'
-                onClick={() => setActiveIndex((activeIndex - 1 + reviews.length) % reviews.length)}
-              />
-              <FontAwesomeIcon
-                icon={faArrowRightLong}
-                fontSize='20px'
-                cursor='pointer'
-                onClick={() => setActiveIndex((activeIndex + 1 + reviews.length) % reviews.length)}
-              />
+              <div className='first' style={{
+                fontSize: '16px',
+                textAlign: 'left',
+                padding: '1rem',
+                borderRadius: '10px',
+                boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
+                lineHeight: 'var(--line-height-default)',
+                width: 'max-content',
+                backgroundColor: activeReview.color
+              }}>
+                {Array.from({ length: activeReview.stars }).map((_, index) => (
+                  <FontAwesomeIcon key={index} icon={faStar} color='gold' />
+                ))}
+                <p>{activeReview.text}</p>
+              </div>
+
+              <div className='opposite' style={{
+                fontSize: '18px',
+                textAlign: 'left',
+                marginLeft: 'auto',
+                marginRight: '0',
+                padding: '1rem',
+                borderRadius: '10px',
+                color: '#fff',
+                boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
+                lineHeight: 'var(--line-height-default)',
+                width: 'max-content',
+                backgroundColor: 'var(--olive-drab)'
+              }}>
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <p>Professionalism at it's peak!</p>
+              </div>
+
+
+              <div className='first' style={{
+                fontSize: '18px',
+                textAlign: 'left',
+                padding: 'var(--default) var(--default)',
+                borderRadius: '10px',
+                boxShadow: 'rgb(0 0 0 / 30%) 0px 12px 14px',
+                lineHeight: 'var(--line-height-default)',
+                width: 'max-content',
+                backgroundColor: activeReview.color
+              }}>
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <FontAwesomeIcon icon={faStar} color='gold' />
+                <p>Best customer service in the area 😊</p>
+              </div>
             </div>
 
           </div>
+          <div className='testimonial__Right'>
+            <div className='right image' style={{
+              position: 'relative'
+            }}>
+              <img src={activeImage} height={450} width={550}
+                aria-hidden alt='happy customer dark'
+                style={{
+                  borderRadius: '25px',
+                  objectFit: 'cover',
+                  objectPosition: 'center'
+                }}
+              />
 
+              <div style={{
+                display: 'flex',
+                gap: '20px',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 'var(--gutter) var(--gutter)',
+
+
+              }}>
+                <FontAwesomeIcon
+                  icon={faArrowCircleLeft}
+                  fontSize='28px'
+                  cursor='pointer'
+                  onClick={() => setActiveIndex((activeIndex - 1 + reviews.length) % reviews.length)}
+                />
+                <FontAwesomeIcon
+                  icon={faArrowCircleRight}
+                  fontSize='28px'
+                  cursor='pointer'
+                  onClick={() => setActiveIndex((activeIndex + 1 + reviews.length) % reviews.length)}
+                />
+              </div>
+
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
